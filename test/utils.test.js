4d60e97cb8e15e6e572d97137514252059fd8492
Meghea Iulian <iulian.meghea@gmail.com>
2020-05-04 19:36:57 +0300

    chore(test): adds more utils tests

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,7 +1,16 @@
 import { assert } from '@open-wc/testing';
-import { search } from '../lib/utils';
+import {
+	array,
+	unarray,
+	without,
+	search,
+	prop,
+	identity
+} from '../lib/utils';
 
 suite('utils', () => {
+	const empty = [],
+		obj = {};
 	suite('search', () => {
 		test('search', () => {
 			assert.deepEqual(
@@ -12,6 +21,31 @@ suite('utils', () => {
 				),
 				[{ text: 'bc' }, { text: 'abc' }]
 			);
+		});
+	});
+	suite('prop', () => {
+		test('prop', () => {
+			assert.equal(prop('b')({ b: 2 }), 2);
+			assert.isUndefined(prop('b')({ c: 4 }));
+		});
+		test('identity', () => {
+			assert.equal(prop(''), identity);
+		});
+	});
+
+	suite('array', () => {
+		test('array', () => {
+			assert.equal(array(empty), empty);
+			assert.equal(array(obj)[0], obj);
+			assert.lengthOf(array(), 0);
+		});
+		test('unarray', () => {
+			assert.equal(unarray(empty), empty);
+			assert.equal(unarray([obj]), obj);
+		});
+		test('without', () => {
+			assert.lengthOf(without(obj)(obj), 0);
+			assert.lengthOf(without([1])([obj, 1]), 1);
 		});
 	});
 });
