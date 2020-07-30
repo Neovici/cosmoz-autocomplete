7cdd2839fc0af66a4c2003854fbcb1da55d9af39
Meghea Iulian <iulian.meghea@gmail.com>
2020-07-30 09:19:03 +0300

    fix: use functional utils from @neovici/cosmoz-utils

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,17 +1,11 @@
 import { assert } from '@open-wc/testing';
 import {
-	array,
-	without,
 	search,
-	prop,
 	strProp,
-	identity,
 	mark
 } from '../lib/utils';
 
 suite('utils', () => {
-	const empty = [],
-		obj = {};
 	suite('search', () => {
 		test('search', () => {
 			assert.deepEqual(
@@ -24,28 +18,6 @@ suite('utils', () => {
 			);
 		});
 	});
-	suite('prop', () => {
-		test('prop', () => {
-			assert.equal(prop('b')({ b: 2 }), 2);
-			assert.isUndefined(prop('b')({ c: 4 }));
-		});
-		test('identity', () => {
-			assert.equal(prop(''), identity);
-		});
-	});
-
-	suite('array', () => {
-		test('array', () => {
-			assert.equal(array(empty), empty);
-			assert.equal(array(obj)[0], obj);
-			assert.lengthOf(array(), 0);
-		});
-		test('without', () => {
-			assert.lengthOf(without(obj)(obj), 0);
-			assert.lengthOf(without([1])([obj, 1]), 1);
-		});
-	});
-
 	suite('mark', () => {
 		test('handles RegExp chars', () => {
 			assert.equal(mark('asd', '[q'), 'asd');
