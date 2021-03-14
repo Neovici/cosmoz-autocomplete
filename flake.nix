aa8b59dae2368c3848f5f8a496cb4c05699afc90
Meghea Iulian <iulian.meghea@gmail.com>
2021-03-14 07:52:28 +0200

    feat(ci): remove karma and cleanup

--- flake.nix
+++ flake.nix
@@ -9,25 +9,24 @@
       let pkgs = nixpkgs.legacyPackages.${system}; in
       rec {
         packages = flake-utils.lib.flattenTree (with pkgs;  rec {
-          tmux-ui = writeShellScriptBin "tmux-ui" ''
-			PROJECT=$(basename $(pwd))
-			tmux at -t $PROJECT || SHELL=bash tmux new -s $PROJECT -n term \; \
-			  send "npm ci" C-m \; splitw -v -p 50 \; \
-			  neww -n tig \; send "tig" C-m \; \
-			  neww -n kak \; send "kak" C-m \; \
-			  selectw -t 1\; selectp -t 1 \;
-          '';
-          test-unit = writeShellScriptBin "test-unit" ''
-	        NIXPKGS_ALLOW_UNFREE=1 nix develop --impure .#testShell -c npm test $@
-          '';
           devShell = mkShell {
             buildInputs = [ nodejs-14_x ];
             shellHook = ''
-              export PATH=$(npm bin):${lib.makeBinPath [tmux-ui test-unit]}:$PATH
+              export PATH=$(npm bin):$PATH
+              export NIXPKGS_ALLOW_UNFREE=1
+
+              tmux-ui() {
+                PROJECT=$(basename $(pwd))
+                tmux at -t $PROJECT || SHELL=bash tmux new -s $PROJECT -n term \; \
+                  send "npm ci" C-m \; splitw -v -p 50 \; \
+                  neww -n tig \; send "tig" C-m \; \
+                  neww -n kak \; send "kak" C-m \; \
+                  selectw -t 1\; selectp -t 1 \;
+              }
             '';
           };
           testShell = mkShell {
-            buildInputs = [ firefox google-chrome ];
+            buildInputs = [ firefox google-chrome geckodriver ];
           };
         });
         defaultPackage = packages.devShell;
