368b22a41c65d1d1f53768ca6a7bdb5c6f0543b4
Meghea Iulian <iulian.meghea@gmail.com>
2022-04-11 22:53:07 +0300

    fix(listbox): correct multi style

--- cosmoz-listbox.js
+++ cosmoz-listbox.js
@@ -6,70 +6,111 @@ import { spreadProps } from '@open-wc/lit-helpers';
 import { useListbox, properties } from './lib/use-listbox';
 import { scrollIntoViewIfNeeded } from './lib/utils';
 
-const Listbox = host => {
-	const { index, items, renderItem, height, itemHeight } = useListbox(host);
+const svg =
+		'data:image/svg+xml,%3Csvg width=\'11\' height=\'8\' viewBox=\'0 0 11 8\' fill=\'none\' xmlns=\'http://www.w3.org/2000/svg\'%3E%3Cpath ' +
+		'd=\'M9.5 1L5.20039 7.04766L1.66348 3.46152\' stroke=\'white\' stroke-width=\'1.5\' stroke-linecap=\'round\' stroke-linejoin=\'round\'/%3E%3C/svg%3E',
+	Listbox = (host) => {
+		const { index, items, renderItem, height, itemHeight } = useListbox(host);
 
-	useEffect(() => {
-		scrollIntoViewIfNeeded(host.shadowRoot.querySelector('[data-index="' + index + '"]:not(:hover)'), host);
-	}, [index]);
+		useEffect(() => {
+			scrollIntoViewIfNeeded(
+				host.shadowRoot.querySelector(
+					'[data-index="' + index + '"]:not(:hover)'
+				),
+				host
+			);
+		}, [index]);
 
-	return html`
-		<style>
-			:host {
-				position: fixed;
-				min-height: ${ itemHeight }px;
-				z-index: 1000;
-				height: ${ height }px;
-				font-family: var(--paper-font-subhead_-_font-family, initial);
-				background: #fff;
-				min-width: 72px;
-				box-shadow: 0 3px 4px 0 rgba(0, 0, 0, 0.14),
-					0 1px 8px 0 rgba(0, 0, 0, 0.12),
-					0 3px 3px -2px rgba(0, 0, 0, 0.4);
-			}
-			.items {
-				position: relative;
-				overflow-y: auto;
-				height: 100%;
-			}
-			.item {
-				font-size: 14px;
-				line-height: ${ itemHeight }px;
-				height: ${ itemHeight }px;
-				padding: 0 20px;
-				box-sizing: border-box;
-				width: 100%;
-				cursor: pointer;
-				overflow: hidden;
-				text-overflow: ellipsis;
-				white-space: nowrap;
-				transition: background 0.2s;
-				color: var(--cosmoz-listbox-color, #101010);
-			}
+		return html` <style>
+				:host {
+					position: fixed;
+					min-height: ${itemHeight}px;
+					z-index: 1000;
+					height: ${height}px;
+					font-family: var(--paper-font-subhead_-_font-family, initial);
+					background: #fff;
+					min-width: 72px;
+					box-shadow: 0 3px 4px 0 rgba(0, 0, 0, 0.14),
+						0 1px 8px 0 rgba(0, 0, 0, 0.12), 0 3px 3px -2px rgba(0, 0, 0, 0.4);
+				}
+				.items {
+					position: relative;
+					overflow-y: auto;
+					height: 100%;
+				}
+				.item {
+					font-size: 14px;
+					line-height: ${itemHeight}px;
+					height: ${itemHeight}px;
+					padding: 0 20px;
+					box-sizing: border-box;
+					width: 100%;
+					cursor: pointer;
+					overflow: hidden;
+					text-overflow: ellipsis;
+					white-space: nowrap;
+					transition: background 0.2s;
+					color: var(--cosmoz-listbox-color, #101010);
+				}
 
-			.item[data-index="${ index }"]{
-				background: var(--cosmoz-listbox-active-color, var(--cosmoz-selection-color, rgba(58, 145, 226, 0.1)));
-			}
+				.item[data-index='${index}'] {
+					background: var(
+						--cosmoz-listbox-active-color,
+						var(--cosmoz-selection-color, rgba(58, 145, 226, 0.1))
+					);
+				}
+				.sizer {
+					position: relative;
+					visibility: hidden;
+					opacity: 0;
+					pointer-events: none;
+					z-index: -1;
+					height: 0;
+					width: auto;
+					padding: 0 20px;
+					overflow: hidden;
+					max-width: inherit;
+					font-size: 14px;
+				}
+				.items > :first-child {
+					line-height: 1;
+				}
 
-			.sizer {
-				position: relative;
-				visibility: hidden;
-				opacity:0;
-				pointer-events: none;
-				z-index: -1;
-				height: 0;
-				width: auto;
-				padding: 0 20px;
-				overflow: hidden;
-				max-width: inherit;
-				font-size: 14px;
-			}
-			.items > :first-child { line-height: 1;}
-			${ host.styles || '' }
-		</style>
-		<div class="items">${ scroll({ items, renderItem }) }</div>`;
-};
+				:host(:not([multi])) .item[aria-selected] {
+					background: var(--cosmoz-listbox-single-selection-color, #dadada);
+				}
+
+				:host([multi]) .item {
+					padding-left: 0;
+				}
+				:host([multi]) .item::before {
+					content: '';
+					font-size: 0;
+					padding: 7.5px;
+					margin: 0 8px;
+					background: #fff;
+					border: 1px solid #d6d6d5;
+					border-radius: 4px;
+					vertical-align: top;
+				}
+				:host([multi]) .item[aria-selected]::before {
+					border-color: #5881f6;
+					background: url('${svg}') #5881f6 no-repeat 50%;
+				}
+				:host([multi]) .sizer {
+					padding-left: 33px;
+				}
+			</style>
+			<div class="items">${scroll({ items, renderItem })}</div>`;
+	};
 
 customElements.define('cosmoz-listbox', component(Listbox));
 
-export const listbox = thru => portal(html`<cosmoz-listbox part="listbox" ...=${ spreadProps(props(properties)(thru)) }>`);
+export const listbox = ({ multi, ...thru }) =>
+	portal(
+		html`<cosmoz-listbox
+			part="listbox"
+			?multi=${multi}
+			...=${spreadProps(props(properties)(thru))}
+		></cosmoz-listbox>`
+	);
