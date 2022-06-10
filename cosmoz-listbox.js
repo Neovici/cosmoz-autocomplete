a7fd0d39b2103cb53ca13dbe7ea0a036522bb837
Meghea Iulian <iulian.meghea@gmail.com>
2022-06-10 08:53:08 +0300

    feat: upgrade to latest lit
    BREAKING CHANGE: Upgrade to latest lit & haunted

--- cosmoz-listbox.js
+++ cosmoz-listbox.js
@@ -1,14 +1,93 @@
 import { html, component, useEffect } from 'haunted';
-import { scroll } from 'lit-virtualizer';
+import { virtualize } from '@lit-labs/virtualizer/virtualize.js';
+import { tagged as css } from '@neovici/cosmoz-utils';
 import { portal } from '@neovici/cosmoz-utils/lib/directives/portal';
+import { spreadProps } from '@neovici/cosmoz-utils/lib/directives/spread-props';
 import { props } from '@neovici/cosmoz-utils/lib/object';
-import { spreadProps } from '@open-wc/lit-helpers';
 import { useListbox, properties } from './lib/use-listbox';
 import { scrollIntoViewIfNeeded } from './lib/utils';
 
 const svg =
 		'data:image/svg+xml,%3Csvg width=\'11\' height=\'8\' viewBox=\'0 0 11 8\' fill=\'none\' xmlns=\'http://www.w3.org/2000/svg\'%3E%3Cpath ' +
 		'd=\'M9.5 1L5.20039 7.04766L1.66348 3.46152\' stroke=\'white\' stroke-width=\'1.5\' stroke-linecap=\'round\' stroke-linejoin=\'round\'/%3E%3C/svg%3E',
+	styles = ({ index, height, itemHeight }) => css`
+		:host {
+			position: fixed;
+			min-height: ${itemHeight}px;
+			z-index: 1000;
+			height: ${height}px;
+			font-family: var(--paper-font-subhead_-_font-family, initial);
+			background: #fff;
+			min-width: 72px;
+			box-shadow: 0 3px 4px 0 rgba(0, 0, 0, 0.14),
+				0 1px 8px 0 rgba(0, 0, 0, 0.12), 0 3px 3px -2px rgba(0, 0, 0, 0.4);
+		}
+		.items {
+			position: relative;
+			overflow-y: auto;
+			contain: layout paint !important;
+			height: 100%;
+		}
+		.item {
+			font-size: 14px;
+			line-height: ${itemHeight}px;
+			height: ${itemHeight}px;
+			padding: 0 20px;
+			box-sizing: border-box;
+			width: 100%;
+			cursor: pointer;
+			text-overflow: ellipsis;
+			white-space: nowrap;
+			transition: background 0.2s;
+			color: var(--cosmoz-listbox-color, #101010);
+		}
+
+		.item[data-index='${index}'] {
+			background: var(
+				--cosmoz-listbox-active-color,
+				var(--cosmoz-selection-color, rgba(58, 145, 226, 0.1))
+			);
+		}
+		.sizer {
+			position: relative;
+			visibility: hidden;
+			opacity: 0;
+			pointer-events: none;
+			z-index: -1;
+			height: 0;
+			width: auto;
+			padding: 0 20px;
+			overflow: hidden;
+			max-width: inherit;
+			font-size: 14px;
+		}
+
+		:host(:not([multi])) .item[aria-selected] {
+			background: var(--cosmoz-listbox-single-selection-color, #dadada);
+		}
+
+		:host([multi]) .item {
+			padding-left: 0;
+		}
+		:host([multi]) .item::before {
+			content: '';
+			font-size: 0;
+			padding: 7.5px;
+			margin: 0 8px;
+			background: #fff;
+			border: 1px solid #d6d6d5;
+			border-radius: 4px;
+			vertical-align: top;
+		}
+		:host([multi]) .item[aria-selected]::before {
+			border-color: #5881f6;
+			/* prettier-ignore */
+			background: url("${svg}") #5881f6 no-repeat 50%;
+		}
+		:host([multi]) .sizer {
+			padding-left: 33px;
+		}
+	`,
 	Listbox = (host) => {
 		const { index, items, renderItem, height, itemHeight } = useListbox(host);
 
@@ -22,86 +101,12 @@ const svg =
 		}, [index]);
 
 		return html` <style>
-				:host {
-					position: fixed;
-					min-height: ${itemHeight}px;
-					z-index: 1000;
-					height: ${height}px;
-					font-family: var(--paper-font-subhead_-_font-family, initial);
-					background: #fff;
-					min-width: 72px;
-					box-shadow: 0 3px 4px 0 rgba(0, 0, 0, 0.14),
-						0 1px 8px 0 rgba(0, 0, 0, 0.12), 0 3px 3px -2px rgba(0, 0, 0, 0.4);
-				}
-				.items {
-					position: relative;
-					overflow-y: auto;
-					height: 100%;
-				}
-				.item {
-					font-size: 14px;
-					line-height: ${itemHeight}px;
-					height: ${itemHeight}px;
-					padding: 0 20px;
-					box-sizing: border-box;
-					width: 100%;
-					cursor: pointer;
-					overflow: hidden;
-					text-overflow: ellipsis;
-					white-space: nowrap;
-					transition: background 0.2s;
-					color: var(--cosmoz-listbox-color, #101010);
-				}
-
-				.item[data-index='${index}'] {
-					background: var(
-						--cosmoz-listbox-active-color,
-						var(--cosmoz-selection-color, rgba(58, 145, 226, 0.1))
-					);
-				}
-				.sizer {
-					position: relative;
-					visibility: hidden;
-					opacity: 0;
-					pointer-events: none;
-					z-index: -1;
-					height: 0;
-					width: auto;
-					padding: 0 20px;
-					overflow: hidden;
-					max-width: inherit;
-					font-size: 14px;
-				}
-				.items > :first-child {
-					line-height: 1;
-				}
-
-				:host(:not([multi])) .item[aria-selected] {
-					background: var(--cosmoz-listbox-single-selection-color, #dadada);
-				}
-
-				:host([multi]) .item {
-					padding-left: 0;
-				}
-				:host([multi]) .item::before {
-					content: '';
-					font-size: 0;
-					padding: 7.5px;
-					margin: 0 8px;
-					background: #fff;
-					border: 1px solid #d6d6d5;
-					border-radius: 4px;
-					vertical-align: top;
-				}
-				:host([multi]) .item[aria-selected]::before {
-					border-color: #5881f6;
-					background: url("${svg}") #5881f6 no-repeat 50%;
-				}
-				:host([multi]) .sizer {
-					padding-left: 33px;
-				}
+				${styles({ index, height, itemHeight })}
 			</style>
-			<div class="items">${scroll({ items, renderItem })}</div>`;
+			<div class="items">
+				<div virtualizer-sizer></div>
+				${virtualize({ items, renderItem, scroller: true })}
+			</div>`;
 	};
 
 customElements.define('cosmoz-listbox', component(Listbox));
--- stories/cosmoz-autocomplete.stories.js
+++ stories/cosmoz-autocomplete.stories.js
@@ -4,76 +4,74 @@ import { colors } from './data';
 
 export default {
 	title: 'Autocomplete',
-	component: 'cosmoz-autocomplete'
+	component: 'cosmoz-autocomplete',
 };
 const css = html`
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500&display=swap');
-cosmoz-autocomplete,
-cosmoz-listbox {
-	font-family: 'Inter', sans-serif;
-}
-</style>
-`,
+		<style>
+			@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500&display=swap');
+			cosmoz-autocomplete,
+			cosmoz-listbox {
+				font-family: 'Inter', sans-serif;
+			}
+		</style>
+	`,
 	basic = () => html`
-		${ css }
+		${css}
 		<cosmoz-autocomplete
-			.label=${ 'Choose color' }
-			.source=${ colors }
-			.textProperty=${ 'text' }
-			.value=${ [colors[0], colors[3]] }
+			.label=${'Choose color'}
+			.source=${colors}
+			.textProperty=${'text'}
+			.value=${[colors[0], colors[3]]}
 		></cosmoz-autocomplete>
 	`,
 	single = () => html`
-		${ css }
+		${css}
 		<cosmoz-autocomplete
-			.label=${ 'Choose color' }
-			.source=${ colors }
-			.limit=${ 1 }
-			.textProperty=${ 'text' }
-			.value=${ colors[2] }
+			.label=${'Choose color'}
+			.source=${colors}
+			.limit=${1}
+			.textProperty=${'text'}
+			.value=${colors[2]}
 		></cosmoz-autocomplete>
 	`,
 	hideEmpty = () => html`
-		${ css }
+		${css}
 		<cosmoz-autocomplete
-			.label=${ 'Choose color' }
-			.source=${ colors }
-			.limit=${ 1 }
-			.textProperty=${ 'text' }
-			.value=${ colors[2] }
+			.label=${'Choose color'}
+			.source=${colors}
+			.limit=${1}
+			.textProperty=${'text'}
+			.value=${colors[2]}
 			hide-empty
 		></cosmoz-autocomplete>
 	`,
 	defaultIndex = () => html`
-		${ css }
+		${css}
 		<cosmoz-autocomplete
-			.label=${ 'Choose color' }
-			.source=${ colors }
-			.limit=${ 1 }
-			.textProperty=${ 'text' }
+			.label=${'Choose color'}
+			.source=${colors}
+			.limit=${1}
+			.textProperty=${'text'}
 			default-index="-1"
 		></cosmoz-autocomplete>
 		<cosmoz-autocomplete
-			.label=${ 'Choose color (single value)' }
-			.source=${ colors.slice(0, 1) }
-			.limit=${ 1 }
-			.textProperty=${ 'text' }
+			.label=${'Choose color (single value)'}
+			.source=${colors.slice(0, 1)}
+			.limit=${1}
+			.textProperty=${'text'}
 			default-index="-1"
 		></cosmoz-autocomplete>
 	`,
 	disabled = () => html`
-		${ css }
+		${css}
 		<cosmoz-autocomplete
 			disabled
-			.label=${ 'Choose color' }
-			.source=${ colors }
-			.limit=${ 1 }
-			.textProperty=${ 'text' }
-			.value=${ colors[2] }
+			.label=${'Choose color'}
+			.source=${colors}
+			.limit=${1}
+			.textProperty=${'text'}
+			.value=${colors[2]}
 		></cosmoz-autocomplete>
 	`;
 
-export {
-	basic, single, hideEmpty, defaultIndex, disabled
-};
+export { basic, single, hideEmpty, defaultIndex, disabled };
