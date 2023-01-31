3999bd6ffe8e939f17a6a59a9f54df64cf25dd01
Paweł <pawel.kuriata@angrynerds.co>
2023-01-31 12:18:14 +0100

    #8568 autocomplete filter changes

--- cosmoz-listbox.js
+++ cosmoz-listbox.js
@@ -1,4 +1,4 @@
-import { html, component, useEffect } from 'haunted';
+import { html, component, useEffect, useCallback, useMemo } from 'haunted';
 import { virtualize } from '@lit-labs/virtualizer/virtualize.js';
 import { tagged as css } from '@neovici/cosmoz-utils';
 import { portal } from '@neovici/cosmoz-utils/directives/portal';
@@ -6,27 +6,55 @@ import { spreadProps } from '@neovici/cosmoz-utils/directives/spread-props';
 import { props } from '@neovici/cosmoz-utils/object';
 import { useListbox, properties } from './lib/use-listbox';
 import { scrollIntoViewIfNeeded } from './lib/utils';
+import '@neovici/cosmoz-input';
 
 const svg =
 		'data:image/svg+xml,%3Csvg width=\'11\' height=\'8\' viewBox=\'0 0 11 8\' fill=\'none\' xmlns=\'http://www.w3.org/2000/svg\'%3E%3Cpath ' +
 		'd=\'M9.5 1L5.20039 7.04766L1.66348 3.46152\' stroke=\'white\' stroke-width=\'1.5\' stroke-linecap=\'round\' stroke-linejoin=\'round\'/%3E%3C/svg%3E',
-	styles = ({ index, height, itemHeight }) => css`
+	closeIcon = html`<svg height="20px" id="Layer_1" style="enable-background:new 0 0 512 512;" version="1.1" viewBox="0 0 512 512" width="20px" 
+	xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path 
+		d="M443.6,387.1L312.4,255.4l131.5-130c5.4-5.4,5.4-14.2,0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4 
+		 	   L256,197.8L124.9,68.3c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4L68,105.9c-5.4,5.4-5.4,14.2,0,19.6l131.5,130L68.4,387.1  
+			   c-2.6,2.6-4.1,6.1-4.1,9.8c0,3.7,1.4,7.2,4.1,9.8l37.4,37.6c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1L256,313.1l130.7,131.1 
+		  	   c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1l37.4-37.6c2.6-2.6,4.1-6.1,4.1-9.8C447.7,393.2,446.2,389.7,443.6,387.1z"/></svg>`,
+	styles = ({ index, searchBarHeight, itemHeight }) => css`
 		:host {
 			position: fixed;
 			min-height: ${itemHeight}px;
 			z-index: 1000;
-			height: ${height}px;
+			height: ${searchBarHeight}px;
 			font-family: var(--paper-font-subhead_-_font-family, initial);
 			background: #fff;
 			min-width: 72px;
 			box-shadow: 0 3px 4px 0 rgba(0, 0, 0, 0.14),
 				0 1px 8px 0 rgba(0, 0, 0, 0.12), 0 3px 3px -2px rgba(0, 0, 0, 0.4);
 		}
+		.searchbar {
+			height: ${itemHeight}px;
+			display: flex;
+			align-items: center;
+			gap: 5px;
+			padding: 0 10px;
+		}
+		.close-button {
+			background: white;
+			border: none;
+			cursor: pointer;
+		}
+		input {
+			flex: 1;
+			border: none;
+			outline: none;
+ 			border-bottom: 2px solid var(--cosmoz-input-focused-color, #3f51b5);
+		}
 		.items {
 			position: relative;
 			overflow-y: auto;
 			contain: layout paint !important;
 			height: 100%;
+		}
+		.items[with-searchbar] {
+			height: calc(100% - 40px);
 		}
 		.item {
 			font-size: 14px;
@@ -98,7 +126,29 @@ const svg =
 		}
 	`,
 	Listbox = (host) => {
-		const { index, items, renderItem, height, itemHeight } = useListbox(host);
+		const { index, items, renderItem, height, itemHeight, setClosed, filterFromDropdown, onFocus, onText, query } = useListbox(host);
+
+		const focusInput = useCallback(() => {
+			host.shadowRoot.getElementById('input')?.focus();
+		}, [])
+
+		const searchBarHeight = useMemo(() => filterFromDropdown ? height + itemHeight : height, 
+			[filterFromDropdown, height, itemHeight]);
+
+		const handleClose = useCallback((e) => {
+			onText({ target: { value: '' }});
+			onFocus(e);
+		}, [onText, onFocus])
+
+		useEffect(() => {
+			focusInput();
+
+			return () => {
+				if(filterFromDropdown) {
+					onText({ target: { value: '' }});
+				}
+			};
+		}, [])
 
 		useEffect(() => {
 			scrollIntoViewIfNeeded(
@@ -110,9 +160,15 @@ const svg =
 		}, [index]);
 
 		return html` <style>
-				${styles({ index, height, itemHeight })}
+				${styles({ index, searchBarHeight, itemHeight })}
 			</style>
-			<div class="items">
+			${filterFromDropdown && 
+				html`<div class="searchbar">
+					<input @input=${onText} @click=${focusInput} @focusin=${onFocus} @focusout=${handleClose} .value=${query ?? ''} type="text" id="input">
+					<button @click=${() => setClosed(true)} class="close-button">${closeIcon}</button>
+				</div>`
+			}
+			<div class="items" ?with-searchbar=${filterFromDropdown}>
 				<div virtualizer-sizer></div>
 				${virtualize({ items, renderItem, scroller: true })}
 			</div>`;
