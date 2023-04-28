1d61916c25ed685d8eb3c2284919a64543330b65
Meghea Iulian <iulian.meghea@gmail.com>
2023-04-28 16:18:04 +0300

    feat: adjust tests

---
+++ test/__snapshots__/cosmoz-autocomplete.test.snap.js
@@ -0,0 +1,218 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["cosmoz-autocomplete-ui render"] = 
+`<cosmoz-input
+  always-float-label=""
+  autocomplete="off"
+  exportparts="input: input-input,label: input-label,line: input-line,error: input-error"
+  id="input"
+  part="input"
+>
+  <slot
+    name="prefix"
+    slot="prefix"
+  >
+  </slot>
+  <slot
+    name="suffix"
+    slot="suffix"
+  >
+  </slot>
+  <cosmoz-autocomplete-chip
+    class="chip"
+    exportparts="chip-text, chip-clear"
+    part="chip"
+    slot="control"
+    title="Item 1"
+  >
+    Item 1
+  </cosmoz-autocomplete-chip>
+  <cosmoz-autocomplete-chip
+    class="chip"
+    exportparts="chip-text, chip-clear"
+    part="chip"
+    slot="control"
+    title="Item 2"
+  >
+    Item 2
+  </cosmoz-autocomplete-chip>
+  <cosmoz-autocomplete-chip
+    class="badge"
+    disabled=""
+    exportparts="chip-text, chip-clear"
+    hidden=""
+    part="chip"
+    slot="control"
+  >
+    <span>
+    </span>
+  </cosmoz-autocomplete-chip>
+</cosmoz-input>
+`;
+/* end snapshot cosmoz-autocomplete-ui render */
+
+snapshots["cosmoz-autocomplete-ui render (limit 1)"] = 
+`<cosmoz-input
+  always-float-label=""
+  autocomplete="off"
+  data-one=""
+  exportparts="input: input-input,label: input-label,line: input-line,error: input-error"
+  id="input"
+  part="input"
+  readonly=""
+>
+  <slot
+    name="prefix"
+    slot="prefix"
+  >
+  </slot>
+  <slot
+    name="suffix"
+    slot="suffix"
+  >
+  </slot>
+  <cosmoz-autocomplete-chip
+    class="chip"
+    exportparts="chip-text, chip-clear"
+    part="chip"
+    slot="control"
+    title="Item 1"
+  >
+    Item 1
+  </cosmoz-autocomplete-chip>
+  <cosmoz-autocomplete-chip
+    class="badge"
+    disabled=""
+    exportparts="chip-text, chip-clear"
+    hidden=""
+    part="chip"
+    slot="control"
+  >
+    <span>
+    </span>
+  </cosmoz-autocomplete-chip>
+</cosmoz-input>
+`;
+/* end snapshot cosmoz-autocomplete-ui render (limit 1) */
+
+snapshots["cosmoz-autocomplete-ui render (listbox)"] = 
+`<cosmoz-input
+  always-float-label=""
+  autocomplete="off"
+  exportparts="input: input-input,label: input-label,line: input-line,error: input-error"
+  id="input"
+  part="input"
+>
+  <slot
+    name="prefix"
+    slot="prefix"
+  >
+  </slot>
+  <slot
+    name="suffix"
+    slot="suffix"
+  >
+  </slot>
+  <cosmoz-autocomplete-chip
+    class="chip"
+    exportparts="chip-text, chip-clear"
+    part="chip"
+    slot="control"
+    title="Item 1"
+  >
+    Item 1
+  </cosmoz-autocomplete-chip>
+  <cosmoz-autocomplete-chip
+    class="badge"
+    disabled=""
+    exportparts="chip-text, chip-clear"
+    hidden=""
+    part="chip"
+    slot="control"
+  >
+    <span>
+      +0
+    </span>
+  </cosmoz-autocomplete-chip>
+</cosmoz-input>
+<disconnect-observer>
+</disconnect-observer>
+`;
+/* end snapshot cosmoz-autocomplete-ui render (listbox) */
+
+snapshots["cosmoz-autocomplete render"] = 
+`<cosmoz-input
+  always-float-label=""
+  autocomplete="off"
+  exportparts="input: input-input,label: input-label,line: input-line,error: input-error"
+  id="input"
+  part="input"
+>
+  <slot
+    name="prefix"
+    slot="prefix"
+  >
+  </slot>
+  <slot
+    name="suffix"
+    slot="suffix"
+  >
+  </slot>
+  <cosmoz-autocomplete-chip
+    class="chip"
+    exportparts="chip-text, chip-clear"
+    part="chip"
+    slot="control"
+    title="Item 1"
+  >
+    Item 1
+  </cosmoz-autocomplete-chip>
+  <cosmoz-autocomplete-chip
+    class="badge"
+    disabled=""
+    exportparts="chip-text, chip-clear"
+    hidden=""
+    part="chip"
+    slot="control"
+  >
+    <span>
+    </span>
+  </cosmoz-autocomplete-chip>
+</cosmoz-input>
+`;
+/* end snapshot cosmoz-autocomplete render */
+
+snapshots["cosmoz-autocomplete render (deselect)"] = 
+`<cosmoz-input
+  autocomplete="off"
+  data-one=""
+  exportparts="input: input-input,label: input-label,line: input-line,error: input-error"
+  id="input"
+  part="input"
+>
+  <slot
+    name="prefix"
+    slot="prefix"
+  >
+  </slot>
+  <slot
+    name="suffix"
+    slot="suffix"
+  >
+  </slot>
+  <cosmoz-autocomplete-chip
+    class="badge"
+    disabled=""
+    exportparts="chip-text, chip-clear"
+    hidden=""
+    part="chip"
+    slot="control"
+  >
+    <span>
+    </span>
+  </cosmoz-autocomplete-chip>
+</cosmoz-input>
+`;
+/* end snapshot cosmoz-autocomplete render (deselect) */
+
