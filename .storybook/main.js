5d248416b21540264ba6cff8762eae8822cfe6e9
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2024-03-21 09:12:44 +0200

    feat: try to add docs to stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,8 @@
 module.exports = {
 	stories: ['../stories/**/*.stories.{js,ts,mdx}'],
+	docs: {
+		//👇 See the table below for the list of supported options
+		autodocs: 'tag',
+		defaultName: 'Documentation',
+	},
 };
