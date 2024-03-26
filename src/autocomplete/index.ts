a81850dc851d0de1b3f38b9c1b0c5dbf48c75b6e
Meghea Iulian <iulian.meghea@gmail.com>
2024-03-26 08:26:21 +0200

    feat(autocomplete): add missing props

--- src/autocomplete/index.ts
+++ src/autocomplete/index.ts
@@ -1,10 +1,5 @@
 import { component, useCallback } from '@pionjs/pion';
-import {
-	Autocomplete,
-	observedAttributes as atts,
-	Props,
-	style,
-} from './autocomplete';
+import { Autocomplete, observedAttributes, Props, style } from './autocomplete';
 import { sheet } from '@neovici/cosmoz-utils';
 
 const Standalone = <I>(host: HTMLElement & Props<I>) => {
@@ -29,14 +24,13 @@ const Standalone = <I>(host: HTMLElement & Props<I>) => {
 	});
 };
 
-const observedAttributes = atts as (keyof Props<unknown>)[],
-	styleSheets = [sheet(style)];
+const styleSheets = [sheet(style)];
 
 customElements.define(
 	'cosmoz-autocomplete-ui',
-	component<Props<unknown>>(Autocomplete, { observedAttributes, styleSheets }),
+	component(Autocomplete, { observedAttributes, styleSheets }),
 );
 customElements.define(
 	'cosmoz-autocomplete',
-	component<Props<unknown>>(Standalone, { observedAttributes, styleSheets }),
+	component(Standalone, { observedAttributes, styleSheets }),
 );
