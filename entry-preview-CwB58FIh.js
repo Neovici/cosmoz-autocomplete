222c88f05625773fdd244a34cf5ffd58239b43ad
megheaiulian <iulian@plumelo.com>
2024-04-01 07:27:41 +0000

    Deploying to gh-pages from @ Neovici/cosmoz-autocomplete@579abe933315d615cb1ad82797a2d8f810ec8855 🚀

---
+++ entry-preview-CwB58FIh.js
@@ -0,0 +1,7 @@
+export { r as render, a as renderToCanvas } from './chunk-FJPRWHXQ-D5xHrl4o.js';
+import './index-Kjm4kNkQ.js';
+import './lit-element-BpxOSt6g.js';
+
+var parameters={renderer:"web-components"};
+
+export { parameters };
