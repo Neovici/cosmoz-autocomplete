2c15a8f554c09dbf5aaf7cf90fa71cf9621deef3
florianstancioiu <florianstancioiu@users.noreply.github.com>
2024-05-07 07:35:18 +0000

    Deploy preview for PR 171 🛫

---
+++ pr/pr-171/entry-preview-CFHclMa0.js
@@ -0,0 +1,7 @@
+export { r as render, a as renderToCanvas } from './chunk-FJPRWHXQ-BzLi6Oyz.js';
+import './index-Kjm4kNkQ.js';
+import './lit-element-CuHsopbm.js';
+
+var parameters={renderer:"web-components"};
+
+export { parameters };
