5efe002e649891890fec020931a2ed94eb83b6b6
megheaiulian <megheaiulian@users.noreply.github.com>
2024-06-03 18:17:15 +0000

    Deploy preview for PR 177 🛫

---
+++ pr/pr-177/entry-preview-D6iZpwNS.js
@@ -0,0 +1,7 @@
+export { r as render, a as renderToCanvas } from './chunk-FJPRWHXQ-E0viTxu0.js';
+import './index-Kjm4kNkQ.js';
+import './lit-element-DB2L7MOw.js';
+
+var parameters={renderer:"web-components"};
+
+export { parameters };
