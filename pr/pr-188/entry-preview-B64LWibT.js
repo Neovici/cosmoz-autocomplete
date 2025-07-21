f16af8a7eb7fda84a0fbf45510a1246a6ffab921
basilgood <basilgood@users.noreply.github.com>
2025-07-21 16:25:37 +0000

    Deploy preview for PR 188 🛫

---
+++ pr/pr-188/entry-preview-B64LWibT.js
@@ -0,0 +1,7 @@
+export { r as render, a as renderToCanvas } from './chunk-FJPRWHXQ-DwrJKsIF.js';
+import './index-Kjm4kNkQ.js';
+import './lit-element-Cn5TfJyM.js';
+
+var parameters={renderer:"web-components"};
+
+export { parameters };
