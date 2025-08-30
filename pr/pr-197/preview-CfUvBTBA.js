4ad48e334e003c72b8613f85721cd83ea6074b8f
cristinecula <cristinecula@users.noreply.github.com>
2025-08-30 06:17:33 +0000

    Deploy preview for PR 197 🛫

---
+++ pr/pr-197/preview-CfUvBTBA.js
@@ -0,0 +1,3 @@
+var parameters={docs:{renderer:async()=>{let{DocsRenderer}=await import('./WithTooltip-V3YHNWJZ-Bt9efOJk.js').then(function (n) { return n.D; });return new DocsRenderer}}};
+
+export { parameters };
