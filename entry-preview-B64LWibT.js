b924d38995bdc57770a4d094de9c9adadb0c671d
basilgood <elsile69@yahoo.com>
2025-09-05 16:28:33 +0000

    Deploying to gh-pages from @ Neovici/cosmoz-autocomplete@6b2f56ef76579df4ebc5d9b317dfb07bc831505c 🚀

--- entry-preview-B64LWibT.js
+++
@@ -1,7 +0,0 @@
-export { r as render, a as renderToCanvas } from './chunk-FJPRWHXQ-DwrJKsIF.js';
-import './index-Kjm4kNkQ.js';
-import './lit-element-Cn5TfJyM.js';
-
-var parameters={renderer:"web-components"};
-
-export { parameters };
