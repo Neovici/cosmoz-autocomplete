17ea59e3b44c7bddd6a38cab5f1ff51a57df9d28
Cristian Necula <virusz@gmail.com>
2025-10-24 15:30:10 +0300

    feat: useProperty value
    The `useProperty` hook allows for efficient state lifting..

--- src/autocomplete/index.ts
+++ src/autocomplete/index.ts
@@ -1,15 +1,25 @@
-import { component, useCallback } from '@pionjs/pion';
+import { component, useCallback, useEffect, useProperty } from '@pionjs/pion';
 import { Autocomplete, observedAttributes, Props, style } from './autocomplete';
 import { sheet } from '@neovici/cosmoz-utils';
 
 const Standalone = <I>(host: HTMLElement & Props<I>) => {
 	const { onChange, onText, ...props } = host;
+	const [value, setValue] = useProperty<I | I[]>('value');
+
+	useEffect(() => {
+		if (host.onChange == null) return;
+		// eslint-disable-next-line no-console
+		console.warn(
+			'onChange is deprecated; use value-changed and lift instead',
+		);
+	}, []);
 
 	return Autocomplete({
 		...props,
+		value,
 		onChange: useCallback(
 			(value: I[], ...args) => {
-				host.value = value;
+				setValue(value);
 				onChange?.(value, ...args);
 			},
 			[onChange],
