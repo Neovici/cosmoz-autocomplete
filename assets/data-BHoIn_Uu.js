bb58bae231e05a5be22e680fec94b3a6adf1e3e6
cristinecula <VirusZ@gmail.com>
2025-12-01 09:59:36 +0000

    Deploying to gh-pages from @ Neovici/cosmoz-autocomplete@f3e166afa9a7bd37f526f0ec281811d032cc81c3 🚀

--- assets/data-BHoIn_Uu.js
+++ assets/data-BHoIn_Uu.js
@@ -1,4 +1,4 @@
-import{T as B,f as Ie,B as Cn,E as pt,m as Fe,x as E,i as En,p as Sn,v as J,r as ht,M as jt,_ as zn}from"./iframe-Cyv2tdxv.js";/**
+import{T as B,f as Ie,B as Cn,E as pt,m as Fe,x as E,i as En,p as Sn,v as J,r as ht,M as jt,_ as zn}from"./iframe-qwpuJ1Z2.js";/**
  * @license
  * Copyright 2017 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
@@ -517,7 +517,6 @@ import{T as B,f as Ie,B as Cn,E as pt,m as Fe,x as E,i as En,p as Sn,v as J,r as
 		white-space: nowrap;
 		flex: auto;
 		min-width: 16px;
-		text-align: center;
 	}
 
 	.clear {
