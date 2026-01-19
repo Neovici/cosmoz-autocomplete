19be340b22306d3594134c8a185a52db7fb2382c
Meghea Iulian <iulian@plumelo.com>
2026-01-19 15:58:09 +0200

    chore: update dependencies to latest safe versions

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,14 @@
 import cfg from '@neovici/cfg/eslint/index.mjs';
 
 export default [
+	{
+		ignores: ['dist/', 'coverage/', 'storybook-static/', '__snapshots__/'],
+	},
 	...cfg,
 	{
-		languageOptions: {
-			ecmaVersion: 'latest',
-			sourceType: 'module',
-		},
 		rules: {
 			'max-lines-per-function': 0,
 			'import/group-exports': 0,
 		},
-		ignores: ['dist/', 'coverage/', 'storybook-static/', '__snapshots__/'],
 	},
 ];
