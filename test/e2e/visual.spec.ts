1e98adbe5f868e3e717c3d2aa134d1d0d71276c4
Husoschi Ionut-Catalin <ionuthusoschi@gmail.com>
2026-01-28 16:22:23 +0200

    feat: test for basic no result and snapshots

--- test/e2e/visual.spec.ts
+++ test/e2e/visual.spec.ts
@@ -49,3 +49,26 @@ test('basic select', async ({ page }) => {
 	await page.getByRole('option', { name: 'Blue' }).click();
 	await expect(page).toHaveScreenshot();
 });
+
+test('basic no result', async ({ page }) => {
+	await page.goto('iframe.html?id=autocomplete--basic&viewMode=story/');
+	await expect(page).toHaveScreenshot();
+
+	await page.click('cosmoz-autocomplete');
+	await expect(page).toHaveScreenshot();
+
+	await page.keyboard.press('Backspace');
+	await expect(page).toHaveScreenshot();
+
+	await page.keyboard.press('Backspace');
+	await expect(page).toHaveScreenshot();
+
+	await page.keyboard.type('Asdf');
+	await expect(page).toHaveScreenshot();
+
+	await page.keyboard.press('Enter');
+	await expect(page).toHaveScreenshot();
+
+	await expect(page.locator('cosmoz-autocomplete')).toContainText('Custom message no results');
+	await expect(page).toHaveScreenshot();
+});
