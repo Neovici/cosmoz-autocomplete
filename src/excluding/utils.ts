13a72fb177de3896daf143c66541d368f41e4c08
Elias Landin <elias.landin@me.com>
2026-03-30 13:59:16 +0200

    feat: extract applyExcluding helper function

--- src/excluding/utils.ts
+++ src/excluding/utils.ts
@@ -2,3 +2,23 @@ import { WrappedItem } from './types';
 
 export const wrap = <I>(item: I): WrappedItem<I> => ({ item, excluded: false });
 export const unwrap = <I>(item: WrappedItem<I>): I => item.item;
+
+export const applyExcluding = <I>(
+	prev: WrappedItem<I>[] | undefined,
+	next: I[] | undefined,
+): WrappedItem<I>[] | undefined => {
+	if (!next) return undefined;
+	if (!prev) return next.map(wrap);
+
+	const results = prev.reduce((nextState, item) => {
+		if (next.includes(item.item)) return [...nextState, item];
+		else if (item.excluded) return nextState;
+		return [...nextState, { ...item, excluded: true }];
+	}, [] as WrappedItem<I>[]);
+
+	const newItems = next
+		.filter((i) => !prev.some((p) => p.item === i))
+		.map(wrap);
+
+	return [...results, ...newItems];
+};
